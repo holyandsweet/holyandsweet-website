6752485fecc6869ec6af370651b8152922b6092f
holyandsweet <holyandsweetm@gmail.com>
2026-05-21 06:27:31 -0500

    force sync fixed imports

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import Footer from "@/components/Footer":
+import Footer from "@/components/Footer";
 
-import NavBar from "@/components/NavBar":
+import NavBar from "@/components/NavBar";
 
 export default function MusicPage() {
   return (
