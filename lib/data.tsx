603cd432ce21c6724abc4c545f38bc444e6f5795
holyandsweet <holyandsweetm@gmail.com>
2026-05-21 18:53:41 -0500

    fix data file with working content

---
+++ lib/data.tsx
@@ -0,0 +1,63 @@
+import {
+  TownIcon,
+  SailboatIcon,
+  DoveIcon,
+} from "@/components/icons";
+
+export const KIDS_SERIES = [
+  {
+    icon: <TownIcon />,
+    name: "Sweetville Friends",
+    desc: "Kids and animals learn faith and kindness",
+    episodes: "8 Episodes",
+    bg: "#082040",
+    color: "#38D4E0",
+  },
+];
+
+export const VIDEOS_KIDS = [
+  {
+    icon: <SailboatIcon size={58} />,
+    bg: "#071830",
+    title: "Noah Obeys God",
+    series: "Bible Story Time",
+  },
+];
+
+export const SONGS = [
+  {
+    icon: <DoveIcon />,
+    bg: "#083040",
+    title: "Still Waters",
+    tag: "Worship",
+    verse: "Psalm 23:2",
+  },
+];
+
+export const PRODUCTS_ADULT = [
+  {
+    icon: <div />,
+    bg: "#E4F4F6",
+    name: "Faith Tee",
+    tag: "Apparel",
+    price: "$32",
+  },
+];
+
+export const PRODUCTS_KIDS = [
+  {
+    icon: <div />,
+    bg: "#DDFAF8",
+    name: "Kids Coloring Book",
+    tag: "Activity",
+    price: "$16",
+  },
+];
+
+export const PLATFORMS = [
+  {
+    logo: <div />,
+    name: "YouTube",
+    sub: "Watch & Subscribe",
+  },
+];
