695708e17532e8bbf191c8a8a8ab21824734bbe2
holyandsweet <holyandsweetm@gmail.com>
2026-05-21 19:06:27 -0500

    add icons file with placeholders

---
+++ components/icons.tsx
@@ -0,0 +1,11 @@
+export const TownIcon = () => (
+  <div style={{ width: 40, height: 40, background: "#38D4E0", borderRadius: 6 }} />
+);
+
+export const SailboatIcon = (props: { size?: number }) => (
+  <div style={{ width: props.size || 40, height: 40, background: "#0A9494", borderRadius: 6 }} />
+);
+
+export const DoveIcon = () => (
+  <div style={{ width: 40, height: 40, background: "#FFFFFF", border: "2px solid #0A9494", borderRadius: 6 }} />
+);
