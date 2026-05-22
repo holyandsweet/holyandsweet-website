f3c65a7195cc52ca8421e09c5d22c9bec2df6355
holyandsweet <holyandsweetm@gmail.com>
2026-05-21 23:20:13 -0500

    updated icons for real'

--- lib/data.tsx
+++ lib/data.tsx
@@ -8,58 +8,124 @@ import {
 
 export const KIDS_SERIES = [
   {
-    icon: "town",
+    icon: <TownIcon />,
     name: "Sweetville Friends",
     desc: "Kids and animals learn faith and kindness",
     episodes: "8 Episodes",
     bg: "#082040",
     color: "#38D4E0",
   },
+  {
+    icon: <ButterflyIcon />,
+    name: "Fruitful Forest",
+    desc: "Exploring the fruits of the spirit in nature",
+    episodes: "12 Episodes",
+    bg: "#0A3060",
+    color: "#18B8CC",
+  }
 ];
 
 export const VIDEOS_KIDS = [
   {
-    icon: "sailboat",
+    icon: <SailboatIcon />,
     bg: "#071830",
     title: "Noah Obeys God",
     series: "Bible Story Time",
   },
+  {
+    icon: <OpenBibleIcon />,
+    bg: "#0A2848",
+    title: "David and Goliath",
+    series: "Bible Story Time",
+  }
 ];
 
 export const SONGS = [
   {
-    icon: "dove",
+    icon: <DoveIcon />,
     bg: "#083040",
     title: "Still Waters",
     tag: "Worship",
     verse: "Psalm 23:2",
   },
+  {
+    icon: <PrayHandsIcon />,
+    bg: "#0A3060",
+    title: "Lord, I Surrender",
+    tag: "Prayer",
+    verse: "James 4:7",
+  },
+  {
+    icon: <SunIcon />,
+    bg: "#114860",
+    title: "Morning Mercies",
+    tag: "Praise",
+    verse: "Lamentations 3:22",
+  }
 ];
 
 export const PRODUCTS_ADULT = [
   {
-    icon: "product",
+    icon: <TShirtIcon />,
     bg: "#E4F4F6",
     name: "Faith Tee",
     tag: "Apparel",
     price: "$32",
   },
+  {
+    icon: <JournalIcon />,
+    bg: "#DAF0F2",
+    name: "Prayer Journal",
+    tag: "Devotional",
+    price: "$24",
+  },
+  {
+    icon: <MugIcon />,
+    bg: "#EEF8F9",
+    name: "Morning Mug",
+    tag: "Home",
+    price: "$18",
+  }
 ];
 
 export const PRODUCTS_KIDS = [
   {
-    icon: "product",
+    icon: <ColoringBookIcon />,
     bg: "#DDFAF8",
     name: "Kids Coloring Book",
     tag: "Activity",
     price: "$16",
   },
+  {
+    icon: <StickerIcon />,
+    bg: "#B4E4EC",
+    name: "Virtue Stickers",
+    tag: "Fun",
+    price: "$8",
+  },
+  {
+    icon: <PlushIcon />,
+    bg: "#DAF0F2",
+    name: "Plush Dove",
+    tag: "Toys",
+    price: "$22",
+  }
 ];
 
 export const PLATFORMS = [
   {
-    logo: "youtube",
+    logo: <YouTubeLogo />,
     name: "YouTube",
     sub: "Watch & Subscribe",
   },
+  {
+    logo: <SpotifyLogo />,
+    name: "Spotify",
+    sub: "Listen Now",
+  },
+  {
+    logo: <AppleMusicLogo />,
+    name: "Apple Music",
+    sub: "Stream Anywhere",
+  }
 ];
